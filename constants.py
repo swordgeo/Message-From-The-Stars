da00340ef8614996c85dffdec2b95331ea20f073
swordgeo <swordgeo1094@yahoo.com>
2024-06-09 18:22:35 -0400

    beginning notes to human.py

--- constants.py
+++ constants.py
@@ -4,3 +4,4 @@
 COMMON_LETTERS = ['A','E','I','L','N','O','R','S','T']
 UNCOMMON_LETTERS = ['B','C','D','F','G','H','M','P','U','W','Y']
 RARE_LETTERS = ['J','K','Q','V','X','Z']
+
---
+++ human.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+# if the score > 0 (positive):
+  # Remove all present letters from consideration of SUSPICION
+  # proceed with main function
+# elif score < 0 (negative):
+  # Remove all letters not present from consideration of SUSPICION
+  # proceed with main function
+# else (score == 0):
+  # Remove all letter present from consideration of TRUST
+
+
+
+# How does the main function proceed? At random?
+# Of all the letters that are currently in the running for each category, brute force by applying every possible combination of available letters and find the configurations that are valid?
+# Ton of combinations to go through, especially at the beginning, but that is one possible option.
+
