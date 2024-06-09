128900d4b3e07b5ee0a68d49e83455ab4408472f
swordgeo <swordgeo1094@yahoo.com>
2024-06-09 18:10:08 -0400

    correctly named common-rare letters

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 
 
 
-GREEN_LETTERS = ['A','E','I','L','N','O','R','S','T']
-GREEN_LETTERS = ['B','C','D','F','G','H','M','P','U','W','Y']
-GREEN_LETTERS = ['J','K','Q','V','X','Z']
+COMMON_LETTERS = ['A','E','I','L','N','O','R','S','T']
+UNCOMMON_LETTERS = ['B','C','D','F','G','H','M','P','U','W','Y']
+RARE_LETTERS = ['J','K','Q','V','X','Z']
